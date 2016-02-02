4a491c9d85037d9a5969500d2691f9bfd8ac1dd9
David <David@10.40.52.109>
2016-02-02 13:48:09 -0500

    initial commit

---
+++ src/testGit/hellotest.java
@@ -0,0 +1,13 @@
+package testGit;
+//test file on how to push to github
+//create repository, right click src folder -> team -> share folder
+//create repository and name it
+
+public class hellotest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+	}
+
+}
